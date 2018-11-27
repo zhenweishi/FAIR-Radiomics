eb63c147515d41884a86370587599d4d87f52963
Zhenwei Shi <zhenweishi88@gmail.com>
2018-11-27 23:14:39 +0100

    clean repository for final version1.x update

--- Run-FAIRR.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Step-1:--------- add dcmqi and dcm2niix to path-------"
-export PATH=$PATH:"$(pwd)/dcmqi/bin"
-export PATH=$PATH:"$(pwd)/dcm2niix"
-
-echo "Step-2:--------- Running FAIR-Radiomics main script-------"
-python3 FAIR-Radiomics.py
